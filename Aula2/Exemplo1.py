236cb5d57cc0655be79179ec7f7e7e652c255b49
João Santos <vitor12822@gmail.com>
2024-10-02 19:09:40 -0300

    Praticas Aula 2
    Exercícios da aula 2 do senac barra, os exercícios de desenvolvimento relacionados a variáves e tipos de dados

---
+++ Aula2/Exemplo1.py
@@ -0,0 +1,10 @@
+'''
+Print -> Comandopara imprimir/escrever natela
+# -> Comentário 
+'''
+#Esses serão os passos do Pirata
+print("andar para direita")
+print("Andar para baixo")
+print("Andar para esquerda")
+print("Ola, Sou o pirata Mokey D. Luffy")
+
