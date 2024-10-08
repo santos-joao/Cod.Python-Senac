4107bd56abdc7b27b2e6ebcc9a8a73418676d019
João Santos <vitor12822@gmail.com>
2024-10-08 10:45:11 -0300

    Teste 1
    Teste 1 para exercicio desafio, alteradas algumas informações para melhor entendimento.

--- Aula5/ExercicioDesafio.py
+++ Aula5/ExercicioDesafio.py
@@ -1,70 +1,75 @@
 #Venda Combo    
 escolha = int(input("-----Boa Noite!!------\nO escolha opções:\n1 -> Hamburguer - custa R$ 10,00\n2 -> Batata Frita - custa R$ 10,00\n3 -> Refrigerante - custa R$ 10,00\n4 -> Combo - R$22.00\n"))
 
-Item1 = "hamburguer"
-Item2 = "batata Frita"
-Item3 = "Refrigerante"
-Item4 = "Combo"
+Hamb_1 = "hamburguer"
+BFrita_2 = "batata Frita"
+Ref_3 = "Refrigerante"
+Combo_4 = "Combo"
 
 #Hamburguer
 if(escolha == 1):
-    print(f"Você escolheu {Item1}")
+    print(f"Você escolheu {Hamb_1}")
     adicional = int(input("Gostaria de adicionar outro Item? \n2 -> Batata Frita - custa R$ 10,00\n3 -> Refrigerante - custa R$ 10,00\n"))
     if (adicional == 2):
-        print(f"Você escolheu {Item1} e {Item2}")
-        oferecercombo = input("Gostaria de adicionar o refrigerente por mais R$2,00?").lower()
+        print(f"Você escolheu {Hamb_1} e {BFrita_2}")
+        oferecercombo = input("Gostaria de adicionar o Refrigerente por mais R$2,00? (Sim ou Não)").lower()
         if(oferecercombo == "sim"):
-            print(f"Seu pedido é {Item1} + {Item2} + {Item3}, totalizando R$22,00")
+            print(f"Seu pedido é {Hamb_1} + {BFrita_2} + {Ref_3}, totalizando R$22,00")
         else:
-            print(f"Seu pedido é {Item1} + {Item2},  totalizando R$20,00")
+            print(f"Seu pedido é {Hamb_1} + {BFrita_2},  totalizando R$20,00")
     elif (adicional == 3):
-        print(f"Você escolheu {Item1} e {Item3} \n")
-        oferecercombo = input("Gostaria de adicionar o Batata Frita por mais R$2,00?").lower()
+        print(f"Você escolheu {Hamb_1} e {Ref_3} \n")
+        oferecercombo = input("Gostaria de adicionar o Batata Frita por mais R$2,00? (Sim ou Não)").lower()
         if(oferecercombo == "sim"):
-            print(f"Seu pedido é {Item1} + {Item2} + {Item3}, totalizando R$22,00")
+            print(f"Seu pedido é {Hamb_1} + {BFrita_2} + {Ref_3}, totalizando R$22,00")
         else:
-            print(f"Seu pedido é {Item1} + {Item3},  totalizando R$20,00")
+            print(f"Seu pedido é {Hamb_1} + {Ref_3},  totalizando R$20,00")
     else:
-        print(f"Seu pedido é {Item1},  totalizando R$10,00")
+        print(f"Seu pedido é {Hamb_1},  totalizando R$10,00")
 
 #Batata Frita
 elif(escolha == 2):
-    print(f"Você escolheu {Item2}")
-    adicional = int(input("Gostaria de adicionar outro Item? \n1 -> Hamburguer - custa R$ 10,00\n2 -> Refrigerante - custa R$ 10,00\n"))
+    print(f"Você escolheu {BFrita_2}")
+    adicional = int(input("Gostaria de adicionar outro Item? \n1 -> Hamburguer - custa R$ 10,00\n3 -> Refrigerante - custa R$ 10,00\n"))
     if (adicional == 1):
-        print(f"Você escolheu {Item1} e {Item2}")
-        oferecercombo = input("Gostaria de adicionar o Batata Frita por mais R$2,00?").lower()
+        print(f"Você escolheu {Hamb_1} e {BFrita_2}")
+        oferecercombo = input("Gostaria de adicionar o Refrigerante por mais R$2,00? (Sim ou Não)").lower()
         if(oferecercombo == "sim"):
-            print(f"Seu pedido é {Item1} + {Item2} + {Item3}, totalizando R$22,00")
+            print(f"Seu pedido é {Hamb_1} + {BFrita_2} + {Ref_3}, totalizando R$22,00")
         else:
-            print(f"Seu pedido é {Item1} + {Item3},  totalizando R$20,00")
+            print(f"Seu pedido é {Hamb_1} + {BFrita_2},  totalizando R$20,00")
     elif (adicional == 3):
-        print(f"Você escolheu {Item2} e {Item3} \n")
-        oferecercombo = input("Gostaria de adicionar o Hamburguer por mais R$2,00?").lower()
+        print(f"Você escolheu {BFrita_2} e {Ref_3} \n")
+        oferecercombo = input("Gostaria de adicionar o Hamburguer por mais R$2,00? (Sim ou Não)").lower()
         if(oferecercombo == "sim"):
-            print(f"Seu pedido é {Item1} + {Item2} + {Item3}, totalizando R$22,00")
+            print(f"Seu pedido é {Hamb_1} + {BFrita_2} + {Ref_3}, totalizando R$22,00")
         else:
-            print(f"Seu pedido é {Item2} + {Item3},  totalizando R$20,00")
+            print(f"Seu pedido é {BFrita_2} + {Ref_3},  totalizando R$20,00")
     else:
-        print(f"Seu pedido é {Item2},  totalizando R$10,00")
+        print(f"Seu pedido é {BFrita_2},  totalizando R$10,00")
 
-#Refrigerante
+#Ref_3rigerante
 elif(escolha == 3):
-    print(f"Você escolheu {Item3}")
-    adicional = int(input("Gostaria de adicionar outro Item? \n2 -> Batata Frita - custa R$ 10,00\n3 -> Refrigerante - custa R$ 10,00\n"))
+    print(f"Você escolheu {Ref_3}")
+    adicional = int(input("Gostaria de adicionar outro Item? \n1 -> Hamburguer - custa R$ 10,00\n2 -> Batata Frita - custa R$ 10,00\n"))
     if (adicional == 2):
-        print(f"Você escolheu {Item1} e {Item2}")
-        oferecercombo = input("Gostaria de adicionar o refrigerente por mais R$2,00?").lower()
+        print(f"Você escolheu {Ref_3} e {BFrita_2}")
+        oferecercombo = input("Gostaria de adicionar o Hamburguer por mais R$2,00? (Sim ou Não)").lower()
         if(oferecercombo == "sim"):
-            print(f"Seu pedido é {Item1} + {Item2} + {Item3}, totalizando R$22,00")
+            print(f"Seu pedido é {Hamb_1} + {BFrita_2} + {Ref_3}, totalizando R$22,00")
         else:
-            print(f"Seu pedido é {Item1} + {Item2},  totalizando R$20,00")
-    elif (adicional == 3):
-        print(f"Você escolheu {Item1} e {Item3} \n")
-        oferecercombo = input("Gostaria de adicionar o Batata Frita por mais R$2,00?").lower()
+            print(f"Seu pedido é {Ref_3} + {BFrita_2},  totalizando R$20,00")
+    elif (adicional == 1):
+        print(f"Você escolheu {Hamb_1} e {Ref_3} \n")
+        oferecercombo = input("Gostaria de adicionar o Batata Frita por mais R$2,00? (Sim ou Não)3").lower()
         if(oferecercombo == "sim"):
-            print(f"Seu pedido é {Item1} + {Item2} + {Item3}, totalizando R$22,00")
+            print(f"Seu pedido é {Hamb_1} + {BFrita_2} + {Ref_3}, totalizando R$22,00")
         else:
-            print(f"Seu pedido é {Item1} + {Item3},  totalizando R$20,00")
+            print(f"Seu pedido é {Hamb_1} + {Ref_3},  totalizando R$20,00")
     else:
-        print(f"Seu pedido é {Item1},  totalizando R$10,00")
+        print(f"Seu pedido é {Ref_3},  totalizando R$10,00")
+#Combo
+elif(escolha == 4):
+    print(f"Você escolheu {Combo_4}")
+else:
+    print("Numereção invalida, tente novamente!")
